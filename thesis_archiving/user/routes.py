adcedd9cacb7183773319c9760998f8c2f411f1f
ralf-t <tabo.ralfmartin@ue.edu.ph>
2021-06-04 16:38:25 +0800

    login page

--- thesis_archiving/user/routes.py
+++ thesis_archiving/user/routes.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, url_for, redirect
+from flask import Blueprint, url_for, redirect, render_template
 
 user = Blueprint('user', __name__, url_prefix="/user")
+
+@user.route("/login")
+def login():
+    return render_template("user/login.html")
 
 @user.route("/read")
 def read():
