fa1100c66a83bbc02f5c3931bd2d26368fd4b7d8
ralf-t <tabo.ralfmartin@ue.edu.ph>
2021-06-10 18:24:55 +0800

    validation boilerplate

--- thesis_archiving/user/routes.py
+++ thesis_archiving/user/routes.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, url_for, redirect, render_template
+from flask import Blueprint, url_for, redirect, render_template, request
+from thesis_archiving.user.validation import LoginSchema, validate_input
+# from pprint import pprint
+
 
 user = Blueprint('user', __name__, url_prefix="/user")
 
-@user.route("/login")
+@user.route("/login", methods=["POST","GET"])
 def login():
-    return render_template("user/login.html")
+
+    valid = {}
+    invalid = {}
+
+    if request.method == "POST":
+        valid, invalid = validate_input(request.form, LoginSchema)
+        
+        if not invalid:
+            return redirect(url_for("thesis.read"))
+
+    return render_template("user/login.html", valid=valid, invalid=invalid)
 
 @user.route("/read")
 def read():
---
+++ thesis_archiving/user/validation.py
@@ -0,0 +1,32 @@
+from marshmallow import Schema, fields, validates, ValidationError, validate, validates_schema 
+
+def validate_empty(data):
+    if len(data.strip()) == 0:
+        raise ValidationError("Field cannot be empty.")
+
+def validate_input(data, schema):
+    '''
+        can only take single dictionary of data
+    '''
+
+    valid = {}
+    invalid = {}
+
+    try:
+        valid = schema().load(data)
+
+    except ValidationError as err:
+        invalid = err.messages
+
+    return valid, invalid
+
+class LoginSchema(Schema):
+    username = fields.Str(required=True, validate=validate_empty)
+    password = fields.Str(required=True, validate=validate_empty)
+
+    @validates_schema
+    def validate_user(self, data, **kwargs):
+
+        # check user encrypted password
+        if not(data["username"] and data["password"]):
+            raise ValidationError("User credentials does not match or exist.")
